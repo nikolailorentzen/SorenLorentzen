667dfebf7ac71b995ebfe9f5657da146732a9436
paaulsens <95236172+paaulsens@users.noreply.github.com>
2024-07-29 17:46:38 +0200

    more setup

--- sorenlorentzentrevare/src/pages/Home.tsx
+++ sorenlorentzentrevare/src/pages/Home.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import './App.css';
+import '../App.css';
 
 const Home = () => {
   return (
     <div className="App">
-        
+        <p> Helloworld </p>
     </div>
   );
 }
